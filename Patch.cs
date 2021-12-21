ef75c4788ff9c93fdc425c3db3fc8841099f3e6e
Lixin Qin <lixinqin@microsoft.com>
2021-12-21 14:12:28 -0800

    feature complete

--- Patch.cs
+++ Patch.cs
@@ -7,63 +7,19 @@ using Verse;
 
 namespace ShowBuildCost
 {
-    //[HarmonyPatch(typeof(MainTabWindow_Research), "DrawUnlockableHyperlinks")]
-    //class Patch1
-    //{
-    //    static void Postfix(Rect rect, ResearchProjectDef project)
-    //    {
-    //        LogHelper.Message($"PostFix MainTabWindow_Research.DrawUnlockableHyperlinks. rect = {rect}, project = {project}");
-    //    }
-    //}
-
-    //[HarmonyPatch(typeof(Widgets), "HyperlinkWithIcon")]
-    //class Patch2
-    //{
-    //    static void Postfix(Rect rect, Dialog_InfoCard.Hyperlink hyperlink)
-    //    {
-    //        LogHelper.Message($"PostFix Widgets.HyperlinkWithIcon. rect = {rect}, hyperlinkDef = {hyperlink.def}");
-    //    }
-    //}
-
-    //[HarmonyPatch(typeof(Dialog_InfoCard.Hyperlink), "ActivateHyperlink")]
-    //class Patch3
-    //{
-    //    static void Postfix(Dialog_InfoCard.Hyperlink __instance)
-    //    {
-    //        LogHelper.Message($"PostFix Dialog_InfoCard.ActivateHyperlink. def = {__instance.def}, thing = {__instance.thing}");
-    //    }
-    //}
-
-    //[HarmonyPatch(typeof(StatsReportUtility), nameof(StatsReportUtility.DrawStatsReport), new[] { typeof(Rect), typeof(Def), typeof(ThingDef) })]
-    //class Patch4
-    //{
-    //    static void Postfix(List<StatDrawEntry> ___cachedDrawEntries)
-    //    {
-    //        LogHelper.Message($"DrawStatsReport.cachedDrawEntries:{System.String.Join(",", ___cachedDrawEntries)}");
-    //    }
-    //}
-
-    //[HarmonyPatch(typeof(Dialog_InfoCard), nameof(Dialog_InfoCard.DoWindowContents))]
-    //class Patch5
-    //{
-    //    static void Postfix()
-    //    {
-    //        LogHelper.Message("DoWindowContents...");
-    //    }
-    //}
-
-    //[HarmonyPatch(typeof(Dialog_InfoCard), "FillCard")]
-    //class Patch6
-    //{
-    //    static void Postfix(Thing ___thing, Def ___def, ThingDef ___stuff)
-    //    {
-    //        LogHelper.Message($"FillCard. thing={___thing}, def={___def}, stuff={___stuff}");
-    //    }
-    //}
-
+    [StaticConstructorOnStartup]
     [HarmonyPatch(typeof(BuildableDef), nameof(BuildableDef.SpecialDisplayStats))]
-    class Patch7
+    class Patch
     {
+        static Patch()
+        {
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            Harmony harmony = new Harmony("Cliffyq.ShowBuildCost");
+            harmony.PatchAll();
+        }
+
         static IEnumerable<StatDrawEntry> Postfix(IEnumerable<StatDrawEntry> originalReturnValues, StatRequest req, BuildableDef __instance)
         {
             foreach (StatDrawEntry entry in originalReturnValues)
@@ -73,7 +29,6 @@ namespace ShowBuildCost
             List<ThingDefCountClass> list = __instance.CostListAdjusted(req.StuffDef, errorOnNullStuff: false);
             if (list.Count > 0)
             {
-                // TODO: detailed descriptions(base, difficulty etc.), meal recipe, cache?
                 var costString = String.Join(", ", list.Select(l => l.Summary));
                 yield return new StatDrawEntry(StatCategoryDefOf.Basics, "Cost", costString, "Cost of this item.", 1100);
             }
